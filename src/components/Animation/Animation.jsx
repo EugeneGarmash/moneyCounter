1843eb9572b60f109d57c63ad4f37c10aebad8cb
Eugene Garmash <e.harmash@zakaz.ua>
2020-11-19 18:30:18 +0200

    Fix forest styles

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import Fire from './Fire/Fire';
 import Forest from './Forest/Forest';
@@ -5,37 +6,48 @@ import Forest from './Forest/Forest';
 // import Rain from './Rain/Rain';
 // import NewYear from './NewYear/NewYear';
 
+import classes from './Animation.module.scss';
+
 const Animation = props => {
 
   const { animationName } = props;
 
-  switch (animationName) {
-    case 'fire':
-      console.log('FIRE'); // постоянный ререндер из-за счётчика
-      return (
-        <Fire />
-      )
-    case 'forest':
-      return (
-        <Forest />
-      )
-    // case 'sea':
-    //   return (
-    //     <Sea />
-    //   )
-    // case 'rain':
-    //   return (
-    //     <Rain />
-    //   )
-    // case 'newYear':
-    //   return (
-    //     <NewYear />
-    //   )
-    default:
-      return (
-        <Fire />
-      )
+  const renderAnimation = (param) => {
+    switch (animationName) {
+      case 'fire':
+        console.log('FIRE'); // постоянный ререндер из-за счётчика
+        return (
+          <Fire />
+        )
+      case 'forest':
+        return (
+          <Forest />
+        )
+      // case 'sea':
+      //   return (
+      //     <Sea />
+      //   )
+      // case 'rain':
+      //   return (
+      //     <Rain />
+      //   )
+      // case 'newYear':
+      //   return (
+      //     <NewYear />
+      //   )
+      default:
+        return (
+          <Fire />
+        )
+    }
   }
+
+  return (
+    <div className={classes.Animation}>
+      {renderAnimation()}
+    </div>
+  )
+
 }
 
 export default Animation;
