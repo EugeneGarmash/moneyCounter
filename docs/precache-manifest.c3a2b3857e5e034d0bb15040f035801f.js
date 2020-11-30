50ee3ec971623c95fceabbbdc51cd15aac88db7f
Eugene Garmash <e.harmash@zakaz.ua>
2020-11-30 11:15:27 +0200

    Copy build to docs to publish on github pages

---
+++ docs/precache-manifest.c3a2b3857e5e034d0bb15040f035801f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7d326b4fdc140aad07032263ee345c",
+    "url": "/moneyCounter/index.html"
+  },
+  {
+    "revision": "243d4703f7e86287da58",
+    "url": "/moneyCounter/static/css/3.be14fdd7.chunk.css"
+  },
+  {
+    "revision": "48f6f5bcb1dcec79595f",
+    "url": "/moneyCounter/static/css/main.3196cb2f.chunk.css"
+  },
+  {
+    "revision": "b93ccca97ce33a8ee23f",
+    "url": "/moneyCounter/static/js/2.53bfecb1.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/moneyCounter/static/js/2.53bfecb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243d4703f7e86287da58",
+    "url": "/moneyCounter/static/js/3.ef28b776.chunk.js"
+  },
+  {
+    "revision": "48f6f5bcb1dcec79595f",
+    "url": "/moneyCounter/static/js/main.74bd1a1a.chunk.js"
+  },
+  {
+    "revision": "a81fc30ff146be38f719",
+    "url": "/moneyCounter/static/js/runtime-main.c80b1505.js"
+  },
+  {
+    "revision": "b82c0c68c09d65ae61d4926c0ccb374b",
+    "url": "/moneyCounter/static/media/bonfire.b82c0c68.wav"
+  },
+  {
+    "revision": "76d2708cf6775841b0ce3cb39620abb0",
+    "url": "/moneyCounter/static/media/forest.76d2708c.wav"
+  },
+  {
+    "revision": "d96b481dbc2b88e6dc49f55e901d232e",
+    "url": "/moneyCounter/static/media/rain.d96b481d.wav"
+  },
+  {
+    "revision": "798420bbaddbf1d71a86939ab9c705d6",
+    "url": "/moneyCounter/static/media/sea.798420bb.wav"
+  }
+]);
